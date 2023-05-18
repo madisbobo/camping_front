54658f373d8cf6aef263edfeda7a4e0c907b1798
Madis Bobrovski <115641439+madisbobo@users.noreply.github.com>
2023-05-18 15:31:58 +0300

    WIP

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,16 +1,44 @@
 <template>
     <div class="container">
-        <div class="row">
-            <div class="col">
-                Column
-            </div>
-            <div class="col">
-                Column
-            </div>
-            <div class="col">
-                Column
+
+        <div class="row justify-content-center">
+            <div class="col col-4 mb-4">
+                <h2>Sisselogimine</h2>
             </div>
         </div>
+
+        <div class="row justify-content-center">
+            <div class="col col-4 mb-4">
+                <label for="username" class="form-label">Kasutajanimi</label>
+                <input type="text" id="username" class="form-control">
+            </div>
+        </div>
+
+        <div class="row justify-content-center">
+            <div class="col col-4 mb-4">
+                <label for="password" class="form-label">Kasutajanimi</label>
+                <input type="text" id="password" class="form-control">
+            </div>
+        </div>
+
+        <div class="row justify-content-center">
+            <div class="col col-4 mb-4">
+                <button type="submit" class="btn btn-dark mb-3">Logi Sisse</button>
+            </div>
+        </div>
+
+        <div class="row justify-content-center">
+            <div class="col col-4 mb-4">
+                <h5>Sul ei ole veel kasutajat?</h5>
+            </div>
+        </div>
+
+        <div class="row justify-content-center">
+            <div class="col col-4 mb-4">
+                <button type="submit" class="btn btn-outline-dark mb-3">Registreeri</button>
+            </div>
+        </div>
+
     </div>
 
 </template>
