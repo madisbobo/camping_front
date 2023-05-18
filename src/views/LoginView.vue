0b73b8e01b57e64ed72affb48f571661b76e8c73
Madis Bobrovski <115641439+madisbobo@users.noreply.github.com>
2023-05-18 14:52:54 +0300

    WIP

---
+++ src/views/LoginView.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                Column
+            </div>
+            <div class="col">
+                Column
+            </div>
+            <div class="col">
+                Column
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: "LoginView.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
