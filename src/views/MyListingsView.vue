f988e0cdfdb0b76a8681011705977e520936ea84
Madis Bobrovski <115641439+madisbobo@users.noreply.github.com>
2023-05-24 16:21:18 +0300

    WIP

--- src/views/MyListingsView.vue
+++ src/views/MyListingsView.vue
@@ -1,11 +1,50 @@
 <template>
-  <h1 class="mt-4">Minu Listingud/Pakkumised</h1>
+    <h1 class="mt-4">Minu Listingud/Pakkumised</h1>
 </template>
 
 <script>
+import router from "@/router";
+
 export default {
-    name: "MyListingsView"
+    name: "MyListingsView",
+    data() {
+        return {
+            message: '',
+            userId: Number(sessionStorage.getItem('userId')),
+            myListingPreviews: [{
+                listingId: 0,
+                listingName: '',
+                price: 0,
+                imageData: '',
+                rating: 0,
+                numberOfRatings: 0
+            }],
+
+
+        }
+
+    },
+    methods: {
+        getMyListingPreview() {
+            this.$http.get("/my-listings", {
+                    params: {
+                        userId: this.userId
+                    }
+                }
+            ).then(response => {
+                alert(this.myListingPreviews.listingName)
+                this.myListingPreviews = response.data
+            }).catch(error => {
+                router.push({name: 'errorRoute'})
+            })
+        },
+    },
+
+    mounted() {
+        this.getMyListingPreview()
+    }
 }
+
 </script>
 
 <style scoped>
